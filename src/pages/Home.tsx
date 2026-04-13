379be7be74348e4a0cda6fb6c7ae70e668d24669
AlbaGG95 <albaganduxe95@gmail.com>
2026-04-13 19:23:33 +0200

    refactor: clean vite template and setup base architecture

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-semibold">Patchworld</h1>
+    </main>
+  )
+}
+
+export default Home
